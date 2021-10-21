f9b3a534331fd84749be4337d98f286cc0751fea
Alexander Strizhachuk <a.strizhachuk@petrovich.ru>
2021-10-21 16:42:24 +0300

    fix: downloader fo EDT

--- jdk.bat
+++ jdk.bat
@@ -2,5 +2,6 @@ docker build ^
   --build-arg DOCKER_USERNAME=%DOCKER_USERNAME% ^
   --build-arg BASE_IMAGE=client-vnc ^
   --build-arg BASE_TAG=%ONEC_VERSION% ^
+  --build-arg OPENJDK_VERSION=15 ^
   -t %DOCKER_USERNAME%/jdk:%ONEC_VERSION% ^
   -f jdk/Dockerfile .
